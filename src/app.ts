28ac35ca722b5b7cc8acbfef5042395eac30bdf7
Farsit-007 <robayatfarsit@gmail.com>
2025-06-20 17:13:10 +0600

    Updated

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,10 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }))
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://estatepro-frontend.vercel.app'],
+  credentials: true
+}));
 app.use('/api',router)
 app.get('/', (req: Request, res: Response) => {
   res.send(`Server is running.`);
